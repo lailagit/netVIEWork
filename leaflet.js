7bbec47ce118b1b5a37c2c3e4aa56f4e8c2f3b95
lailagit <laila.kaichouh@gmail.com>
2016-04-26 12:49:31 +0200

    ajout de la légende dynamique

--- leaflet.js
+++ leaflet.js
@@ -3,9 +3,12 @@ window.onload= function() {
 	var reseau_p;
 	var s_reseau1;
 	var s_reseau2;
+	var n_layer;
+	var div= L.DomUtil.create('div', 'info legend');
+	
 var map = L.map("map", {
         center: new L.LatLng(49.1811, -0.3712),
-		zoom: 15,
+		zoom:16,
 		fullscreenControl: true,//activer le mode plein ecran
 		fullscreenControlOptions: {
         position: 'topleft'
@@ -41,7 +44,7 @@ var carte=L.tileLayer ( geopUrl('7ytqc987zmmjr5g5r3gs1r7p',"GEOGRAPHICALGRIDSYST
 
 		var controlLayers =L.control.layers(baseLayers).addTo(map);
 
-						  			  
+					  			  
 			 var layer=$.getJSON("donnes/Reseau_principal.geojson",function(data){
 				// add GeoJSON layer to the map once the file is loaded
 				reseau_p=L.geoJson(data,{
@@ -57,7 +60,7 @@ var carte=L.tileLayer ( geopUrl('7ytqc987zmmjr5g5r3gs1r7p',"GEOGRAPHICALGRIDSYST
 			
 					}
 				})
-			map.fitBounds(reseau_p.getBounds());
+			 map.fitBounds(reseau_p.getBounds());
 			controlLayers.addOverlay(reseau_p, 'Réseau principal');
 			
 			  });
@@ -101,7 +104,93 @@ var carte=L.tileLayer ( geopUrl('7ytqc987zmmjr5g5r3gs1r7p',"GEOGRAPHICALGRIDSYST
 	       
 			  });
 			  
+			  
+
+			  
+//ajouter la legende
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+   if(n_layer==1){
+        div.innerHTML += '<div id="rp"><i style="background:' + '#ff7800'+'"></i> '+'réseau principal<br></div>';
+   }
+		else if(n_layer==2){div.innerHTML += '<div id="sr1"><i style="background:' + 'blue'+'"></i> '+'sous réseau1<br>';}
+		else div.innerHTML += '<div id="sr2"><i  style="background:' + 'green'+'"></i> '+'sous réseau2<br>';
+			
+		
+
+    return div;
+};
+
+//legend.addTo(map);
+	
+	  
+	map.on('overlayadd', function (eventLayer) {
+
+   if (eventLayer.name === 'Réseau principal') {
+ 
+	   n_layer=1;
+       legend.addTo(this);	  	   
+	   }
+  else if (eventLayer.name === 'Sous réseau1') { 
+	   n_layer=2;
+       legend.addTo(this);	
+  }
+   else { 
+	   n_layer=3;
+       legend.addTo(this);	
+  }
+  });  
+  map.on('overlayremove', function (eventLayer) {
+
+   if (eventLayer.name === 'Réseau principal') {
+var rp = document.getElementById("rp"); 
+div.removeChild(rp); 
+    
+	   }
+	     else if (eventLayer.name === 'Sous réseau1') { 
+ var sr1 = document.getElementById("sr1"); 
+div.removeChild(sr1); 
+ 
+  }
+   else { 
+var sr2 = document.getElementById("sr2"); 
+div.removeChild(sr2);  
+
+  }
+
+  }); 
+	  
+	  
+	  
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
